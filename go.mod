a354688c9564a7fa444c560d2f93def9a94cabe9
kirdatatjana <kirda.tatjana@gmail.com>
2024-04-18 15:02:34 +0100

    finished controller and service

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module follower-service
 
 go 1.22.1
 
-require github.com/neo4j/neo4j-go-driver/v5 v5.19.0
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/neo4j/neo4j-go-driver/v5 v5.19.0
+)
--- service/FollowService.go
+++ service/FollowService.go
@@ -1,5 +1,49 @@
 package service
 
-//follow
-//unfollow
-//all user that follow the user
+import (
+	"follower-service/model"
+	"follower-service/repository"
+	"log"
+)
+
+type UserService struct {
+	repo   *repository.UserRepository
+	logger *log.Logger
+}
+
+func NewUserService(repo *repository.UserRepository, logger *log.Logger) *UserService {
+	return &UserService{
+		repo:   repo,
+		logger: logger,
+	}
+}
+
+func (us *UserService) Follow(user, follower *model.User) error {
+	err := us.repo.Follow(user, follower)
+	if err != nil {
+		us.logger.Println("Error following user:", err)
+		return err
+	}
+	us.logger.Println("User followed successfully")
+	return nil
+}
+
+func (us *UserService) Unfollow(user, unfollower *model.User) error {
+	err := us.repo.Unfollow(user, unfollower)
+	if err != nil {
+		us.logger.Println("Error unfollowing user:", err)
+		return err
+	}
+	us.logger.Println("User unfollowed successfully")
+	return nil
+}
+
+func (us *UserService) GetFollowers(user *model.User) ([]*model.User, error) {
+	followers, err := us.repo.GetFollowers(user)
+	if err != nil {
+		us.logger.Println("Error getting followers:", err)
+		return nil, err
+	}
+	us.logger.Println("Retrieved followers successfully")
+	return followers, nil
+}
